4e7153ed73aae2940f6d281b3ba1e3e9023ec33b
Emrul-Hasan-Emon <74479414+Emrul-Hasan-Emon@users.noreply.github.com>
2021-12-20 11:42:35 +0600

    Create Problem-17.cpp

---
+++ Class-04/Problem-17.cpp
@@ -0,0 +1,18 @@
+Suppose a/b+c=d/e is an equation. What will be the value of a, b, c, d and e. So that for their values equation becomes true. If there are multiple solutions, print any of them.
+For this also we can pick any value for a, b, c, d and e. If we pic a=1, b=1 and c=1 then left side will be 2. As left side is equal to right side, if we take d=2 and e=1 then 
+right side will also be 2. So, the answer can be a=1, b=1, c=1, d=2 and e=1.
+  
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e;
+    // a=1
+    // b=1
+    // c=1
+    // d=2;
+    // e=1
+
+    cout<<"1 1 1 2 1"<<endl;
+}
