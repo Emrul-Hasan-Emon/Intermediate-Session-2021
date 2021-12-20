b36775fc53efdb93e68b529c08ea1e545e0557d8
Emrul-Hasan-Emon <74479414+Emrul-Hasan-Emon@users.noreply.github.com>
2021-12-20 11:41:29 +0600

    Create Problem-16.cpp

---
+++ Class-04/Problem-16.cpp
@@ -0,0 +1,17 @@
+Suppose a/b+c=d is an equation. What will be the value of a, b, c and d. So that for their values equation becomes true. If there are multiple solutions, print any of them.
+For this problem, you can pic any value for a, b, c and d. Suppose if we take a=1, b=1, c=1 then left side will be 2. As left is equal to right side we can pic value 2 for d. 
+In this type of problem you can create your own answer.
+  
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    // a=1
+    // b=1
+    // c=1
+    // d=2;
+
+    cout<<"1 1 1 2"<<endl;
+}
