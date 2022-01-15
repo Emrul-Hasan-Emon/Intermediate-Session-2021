bad7966ae229d2e7cc8f4aaa326a3938acec8764
Emrul-Hasan-Emon <74479414+Emrul-Hasan-Emon@users.noreply.github.com>
2022-01-15 19:24:17 +0600

    Create Problem-28.cpp

---
+++ Class-06/Problem-28.cpp
@@ -0,0 +1,17 @@
+Problem Link : https://open.kattis.com/problems/carrots
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, p;
+    cin>>n>>p;
+
+    for(int i=0; i<n; i++)
+    {
+        string s;
+        cin>>s;
+    }
+    cout<<p<<endl;
+}
